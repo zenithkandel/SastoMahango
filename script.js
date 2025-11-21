660a993653428eb59b83b3b83c4e85ceb3431adb
Zenith Kandel <kandelze123@gmail.com>
2025-11-21 21:18:56 +0545

    Add eye icon toggle for password visibility on click

--- script.js
+++ script.js
@@ -90,6 +90,7 @@ if (togglePassword && password) {
     togglePassword.addEventListener('click', function (e) {
         const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
         password.setAttribute('type', type);
+        this.classList.toggle('fa-eye');
         this.classList.toggle('fa-eye-slash');
     });
 }
