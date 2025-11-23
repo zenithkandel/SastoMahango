b2f091cac00428fd59dc0bc724bcec2aa1e96bab
Zenith Kandel <kandelze123@gmail.com>
2025-11-23 16:30:44 +0545

    Add targetID field to updateItems table for item tracking

--- sql/database.sql
+++ sql/database.sql
@@ -36,6 +36,7 @@ CREATE TABLE items (
 
 CREATE TABLE updateItems (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    targetID INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     category VARCHAR(100) NOT NULL,
     unit VARCHAR(50) NOT NULL,
