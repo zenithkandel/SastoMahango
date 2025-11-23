b65df69891195fc25df570ab9880bb8c4fcbfdf1
Zenith Kandel <kandelze123@gmail.com>
2025-11-23 15:04:47 +0545

    Update navigation link from Insights to Contributors in login.html and request-acess.html

---
+++ API/getContributors.php
@@ -0,0 +1,32 @@
+<?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+
+include 'conn.php';
+
+// Fetch contributors and their contribution count
+$sql = "SELECT c.id, c.full_name, COUNT(i.id) as contributions 
+        FROM contributors c 
+        LEFT JOIN items i ON c.id = i.created_by 
+        GROUP BY c.id 
+        ORDER BY contributions DESC";
+
+$result = $conn->query($sql);
+
+$contributors = array();
+
+if ($result && $result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $contributors[] = [
+            'id' => $row['id'],
+            'name' => $row['full_name'],
+            'contributions' => intval($row['contributions'])
+        ];
+    }
+}
+
+echo json_encode($contributors);
+
+$conn->close();
+?>
